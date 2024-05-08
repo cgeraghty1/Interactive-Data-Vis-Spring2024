e6a7daedbdf3f765205a385b5e6dc7bc165ab6e0
colin_f_gergaghty <colin.geraghty@gmail.com>
2024-05-08 12:13:30 -0400

    Update HTML, CSS, and JS files for 1_3_intro_to_js

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+let count = 0;  
+
+d3.select('#clickButton').on('click', function() {
+    count++;  // increments the counter.
+    d3.select('#counterDisplay').text(count);  //  update  display.
+});
+
+
